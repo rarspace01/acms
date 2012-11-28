cab3bb645c3ff763930fad8c17d70fb08d637aad
LukeSchwoebel@gmail.com <LukeSchwoebel@gmail.com@ffcda290-e998-6680-2e05-0d397e345125>
2012-11-28 21:58:30 +0000

    added revision handling frontend + bugfixing with revisions

--- trunk/lib/classes/language.class.php
+++ trunk/lib/classes/language.class.php
@@ -15,6 +15,8 @@ if(!isset($configSet) OR !$configSet)
 
 class apdLanguage
 {
+	public $localeId;
+
 	/**
 	* function - Constructor
 	* --
@@ -27,6 +29,7 @@ class apdLanguage
 	{
 		$this->mc			= $mainContainer;
 		$this->language		= $this->mc->config['language'];
+		$this->localeId	= $this->mc->database->query("SELECT local_id FROM " . $this->mc->config['database_pref'] . "localisations WHERE local_name = ?", array(array($this->language, "s")))->rows[0]->local_id;
 	}
 	
 	/**
@@ -51,7 +54,7 @@ class apdLanguage
 		else
 		{
 			// if not, check if it is user-generated content
-			$userLocalisationQuery = $this->mc->database->query("SELECT A.local_value AS value FROM " . $this->mc->config['database_pref'] . "localisation_keys AS A, " . $this->mc->config['database_pref'] . "localisations AS B WHERE A.local_key = ? AND A.local_id = B.local_id AND B.local_name = ?", array(array($key, "s"), array($this->language, "s")), array(array("localisation_keys", "local_id", "local_key")));
+			$userLocalisationQuery = $this->mc->database->query("SELECT A.local_value AS value FROM " . $this->mc->config['database_pref'] . "localisation_keys AS A WHERE A.local_key = ? AND A.local_id = ?", array(array($key, "s"), array($this->localeId, "i")), array(array("localisation_keys", "local_id", "local_key")));
 			if(count($userLocalisationQuery->rows) > 0)
 				return $userLocalisationQuery->rows[0]->value;
 		}
